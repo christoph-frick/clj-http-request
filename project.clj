4ada97b431f7df14abcf9268f4cc83d4ef304d0b
Christoph Frick <cf@ofnir.net>
2022-02-08 17:44:18 +0100

    Update deps; use explicit repo coord

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/christoph-frick/clj-http-request"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [cheshire "5.8.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [cheshire/cheshire "5.10.2"]
                  [org.clojure/data.xml "0.0.8"]]
-  :deploy-repositories [["releases" :clojars]])
+  :deploy-repositories {"releases" {:url "https://clojars.org/repo/" :creds :gpg}})
