b2b178ee916135baa8537fbacdaa7e0d1222e706
Christoph Frick <cf@ofnir.net>
2022-06-07 21:50:51 +0200

    Update versions

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/christoph-frick/clj-http-request"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [cheshire/cheshire "5.10.2"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [cheshire/cheshire "5.11.0"]
                  [org.clojure/data.xml "0.0.8"]]
   :deploy-repositories {"releases" {:url "https://clojars.org/repo/" :creds :gpg}})
